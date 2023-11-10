6c8c80c3d6d802c732b5f5106365540450a7e9f4
Marjorie Luize <90302120+AriaGrr@users.noreply.github.com>
2023-11-10 17:31:37 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -2,6 +2,7 @@
 
 typedef struct {
     int prioridade;
+    int estado;
     char categoria[100];
     char descricao[300];
 } Tarefa;
@@ -11,10 +12,29 @@ typedef struct {
     int qtd;
 } ListaDeTarefas;
 
+char* estado(int e);
+
+//Funções principais
 int criarTarefa(ListaDeTarefas *lt);
 int deletarTarefa(ListaDeTarefas *lt);
-int listarTarefa(ListaDeTarefas lt);
+int alterarTarefa(ListaDeTarefas *lt);
 
+// Funções para exportar
+int exportarTarefas(ListaDeTarefas lt,     char exportado[]); // Adicional, não feito.
+int exportarPrioridade(ListaDeTarefas lt, char exportado[]);
+int exportarCategoria(ListaDeTarefas lt, char exportado[]);
+int exportarPCTarefa(ListaDeTarefas lt, char exportado[]);
+
+//Funções de listagem
+int listarTarefas(ListaDeTarefas lt);
+int listarPrioridade(ListaDeTarefas lt);
+int listarCategoria(ListaDeTarefas lt);
+int listarPCTarefa(ListaDeTarefas lt);
+int listarEstado(ListaDeTarefas lt);
+
+
+void menuExportar();
+void menuListagem();
 void printMenu();
 // fwrite
 int salvarLista(ListaDeTarefas lt, char nome[]);
